4ec438cac00d2d170e0c869b8726f15931587187
Mohanraj <MeetMohanraj@gmail.com>
2021-11-12 10:08:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification()
+ { navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/o1sVp0Uqm/model.json', modelReady); }
+
+  function modelReady(){
+      classifier.classify(gotResults);
+  }
+  function gotResults(error,results){
+      if(error){
+          console.error('got results');
+
+      }
+    }
